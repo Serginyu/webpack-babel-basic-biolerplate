e5d0266ce8e70ec2cf199097bc7e8085b73c34b7
Sergio Novo <sergionovo1997@gmail.com>
2017-10-08 20:03:33 +0200

    v1.0.0

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: path.join(__dirname, './src'),
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loaders: ['babel-loader'],
+            exclude: /node_modules/
+        }]
+    },
+    plugins: [
+        /*new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })*/
+    ],
+    stats: {
+        errorDetails: true, //this does show errors
+        colors: true,
+        modules: true,
+        reasons: true
+    }
+};
